a93147589dead51e72038b7c99ce8f7144b10766
vietthang123 <57828135+vietthang123@users.noreply.github.com>
2022-05-25 14:59:23 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h2>Start editing</h2>
-      <h2>Start editing</h2>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateSubmit = this.updateSubmit.bind(this);
+    this.input = React.createRef();
+  }
+  updateSubmit(event) {
+    alert("You have entered the UserName and CompanyName successfully.");
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.updateSubmit}>
+        <h1>Uncontrolled Form</h1>
+        <label>
+          Name:
+          <input type="text" ref={this.input} />
+        </label>
+        <label>
+          CompanyName:
+          <input type="text" ref={this.input} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
+export default App;
